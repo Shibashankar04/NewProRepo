a5da4ad2cac81ca58325e49007e20a7c737a88fe
Shibashankar04 <shibashankar04@gmail.com>
2019-08-09 12:10:40 +0530

    first commit

---
+++ src/test/java/com/may/maven/SecondTest.java
@@ -0,0 +1,22 @@
+package com.may.maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SecondTest {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver.exe");
+
+		WebDriver driver=new ChromeDriver();
+
+		driver.get("https:/www.google.com");
+
+		String title=driver.getTitle();
+
+		System.out.println(title);
+
+		driver.quit();
+	}
+
+}
