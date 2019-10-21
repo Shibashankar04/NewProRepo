4c480c7973deb1cd036d736c385c50b61e10ce6f
Shibashankar04 <shibashankar04@gmail.com>
2019-10-21 10:44:50 +0530

    new commit

--- src/test/java/com/may/maven/FirstTest.java
+++ src/test/java/com/may/maven/FirstTest.java
@@ -7,7 +7,7 @@ public class FirstTest {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Stark\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		
 		WebDriver driver=new ChromeDriver();
 		
--- src/test/java/com/may/maven/SecondTest.java
+++ src/test/java/com/may/maven/SecondTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SecondTest {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\Downloads\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Stark\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver=new ChromeDriver();
 
