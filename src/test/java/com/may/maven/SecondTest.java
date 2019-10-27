b63bf5d0497676df44fd1db9c25afed1adac4fbe
Shibashankar04 <shibashankar04@gmail.com>
2019-10-27 10:29:51 +0530

    added new config: maven with testng

--- src/test/java/com/may/maven/SecondTest.java
+++ src/test/java/com/may/maven/SecondTest.java
@@ -2,10 +2,13 @@ package com.may.maven;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class SecondTest {
-
-	public static void main(String[] args) {
+	
+	
+	@Test
+	public static void findName() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Stark\\Downloads\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver=new ChromeDriver();
